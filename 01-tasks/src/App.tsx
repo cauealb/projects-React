b456ec828d23139e2a1fc4887601db4ca9ee0f54
Cauê Alves <cauealvesb4@gmail.com>
2024-12-12 20:27:00 -0300

    Criando a pasta de componentes + Configurando layout global

--- 01-tasks/src/App.tsx
+++ 01-tasks/src/App.tsx
@@ -1,9 +1,13 @@
-
+import app from './app.module.css'
 
 function App() {
 
   return (
     <>
+      <section className={app.hero}>
+        <h1>Hello</h1>
+        h1
+      </section>
       
     </>
   )
