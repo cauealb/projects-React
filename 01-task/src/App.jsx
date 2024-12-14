2e43cafa9ca1c7cf5a0d04584c0692afcff4231b
Cauê Alves <cauealvesb4@gmail.com>
2024-12-14 10:24:32 -0300

    Colocando o componente que lista as taks no compenente que insere as task

--- 01-task/src/App.jsx
+++ 01-task/src/App.jsx
@@ -3,13 +3,11 @@ import { FormTaks } from './Components/FormTaks'
 import { ListTaks } from './Components/ListTaks'
 
 function App() {
-
   return (
     <>
       <section className={appStyle.wapper}>
         <h1>Gerenciador de Tarefas</h1>
         <FormTaks />
-        <ListTaks />
       </section>
     </>
   )
--- 01-task/src/Components/FormTaks.jsx
+++ 01-task/src/Components/FormTaks.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import form from './FormTaks.module.css'
+import { ListTaks } from './ListTaks'
 
 export function FormTaks (){
+    
     return (
-        <form className={form.form}>
-            <input type="text" placeholder='Beber àgua...' required/>
+        <section className={form.sect}>
+
+            <form className={form.form}>
+            <input name='task' type="text" placeholder='Beber àgua...' required/>
             <button type='submit'>Enviar</button>
         </form>
+
+        <ListTaks />
+        </section>
     )
 }
