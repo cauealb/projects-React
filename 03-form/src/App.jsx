5895785f1ac2c6f3d5f5358e79cc07ee343a3a81
Cauê Alves <cauealvesb4@gmail.com>
2024-12-23 22:39:33 -0300

    Criando funcionalidade de incluir um login

--- 03-form/src/App.jsx
+++ 03-form/src/App.jsx
@@ -3,18 +3,21 @@ import { ThemeProvider } from "styled-components";
 import { DefaultStyledTheme } from "./theme/DefaultStyled";
 import { Form } from "./Components/Form/Form";
 import { List } from "./Components/List/ListLogin";
-
-const api = [
-  1, 2, 3
-]
+import { useState } from "react";
 
 function App() {
+  const [itemList, setItemList] = useState([])
+
+  function SubmitItens() {
+    event.preventDefault()
+    setItemList([...itemList, itemList])
+  }
 
   return (
     <>
       <ThemeProvider theme={DefaultStyledTheme}>
-        <Form />
-        {api.map(item => {
+        <Form submit={SubmitItens}/>
+        {itemList.map(item => {
           return <List /> 
         })}
         <GlobalCSS />
--- 03-form/src/Components/Form/Form.jsx
+++ 03-form/src/Components/Form/Form.jsx
@@ -1,14 +1,14 @@
 import { StyledForm, StyledLabel, StyledInput, StyledButton } from "./Form.s";
 
-export function Form({changeUser, changePassword}) {
+export function Form({submit}) {
     return (
         <>
-            <StyledForm>
+            <StyledForm onSubmit={submit}>
                 <StyledLabel htmlFor="user">User</StyledLabel>
-                <StyledInput onChange={changeUser} type="text" id="user" placeholder="exemple123" autoFocus autoComplete="off" required min={5}/>
+                <StyledInput type="text" id="user" placeholder="exemple123" autoFocus autoComplete="off" required min={5}/>
 
                 <StyledLabel htmlFor="password">Password</StyledLabel>
-                <StyledInput onChange={changePassword} type="text" id="password" placeholder="password12345" autoComplete="off"  required min={5}/>
+                <StyledInput type="text" id="password" placeholder="password12345" autoComplete="off"  required min={5}/>
 
                 <StyledButton type="submit">Send</StyledButton>
             </StyledForm>
