7c300066f516c0733a8e5e2ae0c633f9c356a3c1
Cauê Alves <cauealvesb4@gmail.com>
2024-12-23 21:27:32 -0300

    Finalizando o Form

--- 03-form/src/Components/Form/Form.jsx
+++ 03-form/src/Components/Form/Form.jsx
@@ -1,16 +1,16 @@
-import { StyledForm, StyledLabel, StyledInput } from "./Form.s";
+import { StyledForm, StyledLabel, StyledInput, StyledButton } from "./Form.s";
 
 export function Form() {
     return (
         <>
             <StyledForm>
                 <StyledLabel htmlFor="user">User</StyledLabel>
-                <StyledInput type="text" id="user" placeholder="exemple123" required min={5}/>
+                <StyledInput type="text" id="user" placeholder="exemple123" autoFocus autoComplete="off" required min={5}/>
 
                 <StyledLabel htmlFor="password">Password</StyledLabel>
-                <StyledInput type="text" id="password" placeholder="password12345" required min={5}/>
+                <StyledInput type="text" id="password" placeholder="password12345" autoComplete="off"  required min={5}/>
 
-                <button type="submit">Enviar</button>
+                <StyledButton type="submit">Send</StyledButton>
             </StyledForm>
         </>
     )
--- 03-form/src/Components/Form/Form.s.js
+++ 03-form/src/Components/Form/Form.s.js
@@ -12,23 +12,37 @@ export const StyledForm = styled.form`
     background-color: ${props => props.theme['--gray-900']};
     border-radius: 8px;
     box-shadow: 0 7px 10px ${props => props.theme['--gray-700']};
-
-    input {
-        margin-top: 0.20rem
-    }
 `;
 
 export const StyledLabel = styled.label`
-    margin-top: 1rem;
+    margin-top: 1.4rem;
     background-color: transparent;
     color: inherit;
 `;
 
 export const StyledInput = styled.input`
+    margin-top: 0.20rem;
     outline: none;
     border-radius: 4px;
     color: inherit;
     padding: 0.50rem;
     font-size: 1.3rem;
     border: none;
+    border-top: 1px solid transparent;
+    border-bottom: 1px solid transparent;
+
+    &:focus {
+        border-bottom: 1px solid ${props => props.theme['--green-500']}
+    }
+`;
+
+export const StyledButton = styled.button`
+    cursor: pointer;
+    border: none;
+    border-radius: 8px;
+    background-color: ${props => props.theme['--green-300']};
+    padding: 0.75rem 3rem;
+    margin-top: 1.2rem;
+    width: fit-content;
+    align-self: center
 `;
