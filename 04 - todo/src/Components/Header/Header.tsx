51af10cda31797b9cc742006c52c2a220b369df7
Cauê Alves <cauealvesb4@gmail.com>
2025-01-05 08:41:25 -0300

    style: Trocando a logo para um svg + Mudando a posição da logo

--- 04 - todo/src/Components/Header/Header.tsx
+++ 04 - todo/src/Components/Header/Header.tsx
@@ -1,19 +1,15 @@
-import { Rocket } from 'phosphor-react'
+import { PlusCircle } from 'phosphor-react'
 import styleHeader from './HeadeStyle.module.css'
+import logoTodo from  '../../assets/Logo todo.svg'
 
 export function Header() {
     return (
-        <>
+        <>  
             <header className={styleHeader.header}>
-                <div className={styleHeader.hero}>
-                    <Rocket size={34} color="#8284FA"/>
-                    <div className={styleHeader.logo}>
-                        <span className={styleHeader.logo_1}>to</span><span className={styleHeader.logo_2}>do</span>
-                    </div>
-                </div>
+                <img className={styleHeader.logo} src={logoTodo} alt="Logo da todo" />   
                 <form>
                     <input className={styleHeader.taskname} type="text" placeholder="Adicione uma nova tarefa"/>
-                    <button className={styleHeader.btn}>Criar</button>
+                    <button className={styleHeader.btn}>Criar <PlusCircle size={15}/></button>
                 </form>
             </header>
         </>
