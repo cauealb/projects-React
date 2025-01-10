a4fa59c134535f919fa0e02d2a718ac68e54cb2e
Cauê Alves <cauealvesb4@gmail.com>
2025-01-10 20:08:41 -0300

    feat: Criando ternarios para as listas

--- 04 - todo/src/Components/ListTask/ListTask.tsx
+++ 04 - todo/src/Components/ListTask/ListTask.tsx
@@ -1,5 +1,5 @@
 import styleList from './ListTask.module.css'
-import { Trash } from 'phosphor-react'
+import { Trash, CheckCircle, Circle } from 'phosphor-react'
 
 interface ListTaskProps {
     id: number
@@ -9,7 +9,7 @@ interface ListTaskProps {
     handleCompleteTask: (id: number) => void
 }
 
-export function ListTask ({content, id, deleteTask, handleCompleteTask}: ListTaskProps) {
+export function ListTask ({content, id, isComplete,deleteTask, handleCompleteTask}: ListTaskProps) {
 
     function handleClickDelete() {
         deleteTask(id)
@@ -19,13 +19,18 @@ export function ListTask ({content, id, deleteTask, handleCompleteTask}: ListTas
         handleCompleteTask(id)
     }
 
+    // function 
+
     return (
         <>  
             <div className={styleList.content__task}>
-                <form className={styleList.radio}>
-                    <input onClick={CompleteTask} type="checkbox" />
-                </form>
-                <p>{content}</p>
+                {isComplete ? (
+                    <CheckCircle className={styleList.input} size={20} color='#5E60CE'/>
+                ) : (
+                    <Circle className={styleList.input} onClick={CompleteTask} size={20} color='#4EA8DE'/>
+                )}
+
+                <p className={isComplete ? styleList.content_risck : styleList.content_norisck}>{content}</p>
                 <span onClick={handleClickDelete} ><Trash size={20} /></span>
             </div>
         </>
