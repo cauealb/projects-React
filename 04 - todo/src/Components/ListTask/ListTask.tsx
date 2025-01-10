c075e94353f8c966b4d788eff0d3d16f6e3998cd
Cauê Alves <cauealvesb4@gmail.com>
2025-01-10 20:18:55 -0300

    style: Colocando svg como checkbox

--- 04 - todo/src/Components/ListTask/ListTask.tsx
+++ 04 - todo/src/Components/ListTask/ListTask.tsx
@@ -1,5 +1,10 @@
 import styleList from './ListTask.module.css'
-import { Trash, CheckCircle, Circle } from 'phosphor-react'
+import { Trash } from 'phosphor-react'
+// import CircleSVG from '../../assets/Layer 1.svg'
+// import CircleCheckSVG from '../../assets/Layer 2.svg'
+import CircleSVG from '../../assets/checkFalse.svg'
+import CircleCheckSVG from '../../assets/checkTrue.svg'
+
 
 interface ListTaskProps {
     id: number
@@ -25,9 +30,9 @@ export function ListTask ({content, id, isComplete,deleteTask, handleCompleteTas
         <>  
             <div className={styleList.content__task}>
                 {isComplete ? (
-                    <CheckCircle className={styleList.input} size={20} color='#5E60CE'/>
+                    <img onClick={CompleteTask} src={CircleSVG}/>
                 ) : (
-                    <Circle className={styleList.input} onClick={CompleteTask} size={20} color='#4EA8DE'/>
+                    <img onClick={CompleteTask} src={CircleCheckSVG} />
                 )}
 
                 <p className={isComplete ? styleList.content_risck : styleList.content_norisck}>{content}</p>
