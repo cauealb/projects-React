6de395fbc141232f0af8c0586d419d070a712666
Cauê Alves <cauealvesb4@gmail.com>
2025-01-15 06:35:30 -0300

    style: Melhorando o posicionamento do Hero

--- 05-coffe-delivey/src/Routes/Components/Hero/Hero.tsx
+++ 05-coffe-delivey/src/Routes/Components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
     return (
         <>
             <SectionPrincipal>
-                    <img src={imgCoffe} alt="Café feito pela Coffe Delivery" />
+                <img src={imgCoffe} alt="Café feito pela Coffe Delivery" />
                 <div>
                     <h1>Encontre o café perfeito para qualquer hora do dia</h1>
                     <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
--- 05-coffe-delivey/src/Routes/Components/Hero/HeroStyle.ts
+++ 05-coffe-delivey/src/Routes/Components/Hero/HeroStyle.ts
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const SectionPrincipal = styled.section`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     flex-direction: row-reverse;
-    background-color: teal
+    width: 75%;
+    max-width: 1440px;
+    margin: auto;
+    padding: 0.75rem;
+    box-sizing: border-box;
+
+    & h1 {
+      font-family: "Baloo 2", sans-serif;
+      font-size: 3rem;
+      margin-top: 0;
+    }
 `;
