c5b6648c249164cc7c0379c94e31cc08b54dcc9c
Cauê Alves <cauealvesb4@gmail.com>
2025-06-15 11:00:12 -0300

    style: Finalizando o nome da empresa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react"
+import Header from "./components/Header/Header";
 
 export default function App() {
   return(
-    <h1>
-      Hello Word
-    </h1>
+    <Header />
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import useHeader from "./useHeader"
+import logo from '../../assets/Logo.svg'
+import logoTitle from '../../assets/ToDo List.svg'
+import './style/Header.css'
 
 export default function Header() {
     return(
-        <header>
-
+        <header class="hero">
+            <div class="nameCompany">
+                <img src={logo} alt="Logo do ToDoList" />
+                <img src={logoTitle} />
+            </div>
+            <div>
+                <div class="taskCreate">
+                    <p>Tarefas criadas</p>
+                    <p>0</p>    
+                </div>
+                <div class="taskFinish">
+                    <p>Concluídas</p>
+                    <p>0</p>  
+                </div>
+            </div>
         </header>
     )
 }
