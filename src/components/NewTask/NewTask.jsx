587a1c27980fd55f80a83a7601442398bbaf4d82
Cauê Alves <cauealvesb4@gmail.com>
2025-06-17 19:39:52 -0300

    feat: Importando as variáveis de contexto

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,11 +1,19 @@
 import { PlusIcon } from "@phosphor-icons/react";
 import './style/NewTask.css';
+import { useContext } from "react";
+import { Context } from "../../AppContext/AppContext";
 
 export default function NewTask() {
-    return (
+    const { newTask } = useContext(Context)
+
+    if(!newTask) {
+        return (
         <button class="newTask">
             <PlusIcon color="#C257A4" size={20} />
             <p>Nova tarefa</p>
         </button>
     )
+    } else {
+
+    }
 }
