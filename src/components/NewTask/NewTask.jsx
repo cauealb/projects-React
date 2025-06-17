88184c7ee6bc0a697d2a3d82bb923f240966aebc
Cauê Alves <cauealvesb4@gmail.com>
2025-06-17 19:18:44 -0300

    feat: Iniciando estilização do botão

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -3,7 +3,7 @@ import './style/NewTask.css';
 
 export default function NewTask() {
     return (
-        <button>
+        <button class="newTask">
             <PlusIcon color="#C257A4" size={20} />
             Nova tarefa
         </button>
