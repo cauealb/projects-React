030844cf0626f1c68cb0aca99074bc8779356760
Cauê Alves <cauealvesb4@gmail.com>
2025-06-17 19:37:25 -0300

    feat: Incluindo novas variáveis para o contexto

--- src/AppContext/AppContext.jsx
+++ src/AppContext/AppContext.jsx
@@ -1,19 +1,25 @@
 import { createContext } from "react";
 import useHeader from "../components/Header/useHeader";
+import useNewTask from "../components/NewTask/useNewTask";
 
 export const Context = createContext({
     taskFinish: 0,
-    handleTaskFinish: () => {}
+    handleTaskFinish: () => {},
+    newTask: false,
+    handleCreateNewTask: () => {}
 })
 
 export default function AppContext({ children }) {
     const { taskFinish, handleTaskFinish } = useHeader()
+    const { newTask, handleCreateNewTask } = useNewTask()
     
     return (
         <Context.Provider value={
             {
                 taskFinish, 
-                handleTaskFinish
+                handleTaskFinish,
+                newTask,
+                handleCreateNewTask
             }
         }>
             {children}
