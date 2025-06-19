9215c747928bbdea473796ef4c7326300028de0d
Cauê Alves <cauealvesb4@gmail.com>
2025-06-19 17:17:37 -0300

    feat: Finalizando lógica do Header

--- src/components/Header/useHeader.js
+++ src/components/Header/useHeader.js
@@ -5,4 +5,16 @@ export default function useHeader() {
     const [taskCreated, setTaskCreated] = useState(0)
     const [completedTask, setCompletedTask] = useState(0)
 
+    function incrementTask() {
+        setTaskCreated(state => state + 1)
+    }
+
+    function incrementCompletedTask() {
+        setCompletedTask(state => state + 1)
+    }
+
+    return {
+        incrementTask,
+        incrementCompletedTask
+    }
 }
