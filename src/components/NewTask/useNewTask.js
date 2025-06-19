a40d02c72de1f3c71407cae6115ce0127159a2d7
Cauê Alves <cauealvesb4@gmail.com>
2025-06-19 16:13:22 -0300

    feat: Incluindo lógica de criar um Task nova

--- src/components/NewTask/useNewTask.js
+++ src/components/NewTask/useNewTask.js
@@ -2,10 +2,21 @@ import { useState } from "react"
 
 
 export default function useNewTask() {
+    const [listTasks, setListTasks] = useState([])
     const [newTask, setNewTask] = useState(false)
     
     function handleCreateNewTask() {
         setNewTask(state => state ? false : true)
+    }
+
+    function handleNewTask() {
+        const newTask = {
+            id: new Date().getTime,
+            text: 'blablabla',
+            finish: false
+        }
+
+        setListTasks(state => [...state, newTask])
     }
 
     return {
