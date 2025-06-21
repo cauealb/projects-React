66c95ba113375d2d94913e2647345084a7a97b94
Cauê Alves <cauealvesb4@gmail.com>
2025-06-21 11:34:04 -0300

    feat: Removendo lógica do Task

--- src/components/Task/useTask.js
+++ src/components/Task/useTask.js
@@ -1,16 +1,4 @@
-import { useState } from "react";
-
 
 export default function useTask() {
-    const [taskFinish, setTaskFinish] = useState(true);
-
-    function handleTaskFinish() {
-        setTaskFinish(state => state ? false : true)
-    }
-
-    return{
-        taskFinish,
-        handleTaskFinish
-    }
 
 }
