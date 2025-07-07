e0783b97c78399367cbd0857ccf67791a464d7bf
Cauê Alves <cauealvesb4@gmail.com>
2025-07-07 20:56:26 -0300

    Começando a tipar o context

---
+++ 2 - App de compras/app-compras/src/components/AppContext/AppContext.tsx
@@ -0,0 +1,16 @@
+import { createContext } from "react"
+import { ThemeProvider } from "styled-components"
+
+interface PropsContext {
+    theme: 'light' | 'dark'
+}
+
+export const Context<>
+
+export default function AppContext() {
+    return (
+        <>
+        
+        </>
+    )
+}
