916f175152d891c80a98a198de74593e511683b7
Cauê Alves <cauealvesb4@gmail.com>
2025-07-07 21:31:06 -0300

    Começando a estilização do Header

--- 2 - App de compras/app-compras/src/App.tsx
+++ 2 - App de compras/app-compras/src/App.tsx
@@ -1,11 +1,12 @@
 import AppContext from "./components/AppContext/AppContext"
+import Header from "./components/Header/Header"
 
 export default function App() {
   
 
   return (
       <AppContext>
-        <h1>Hello Word!</h1>
+        <Header />
       </AppContext>
   )
 }
--- 2 - App de compras/app-compras/src/components/Header/Header.tsx
+++ 2 - App de compras/app-compras/src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
-
+import { List } from 'phosphor-react'
+import { ContainerHeader } from './style/Header'
 
 export default function Header() {
     return (
-        <>
-            
-        </>
+        <ContainerHeader>
+            <div>
+                <List />
+                <h1>Testeeeeeeeeee</h1>
+            </div> 
+        </ContainerHeader>
     )
 }
