6d713be5c312bff2b0c6a417cae4aa1a96810d25
Cauê Alves <cauealvesb4@gmail.com>
2025-07-08 20:06:46 -0300

    Finalizando tipegm do theme

--- 2 - App de compras/app-compras/src/components/AppContext/AppContext.tsx
+++ 2 - App de compras/app-compras/src/components/AppContext/AppContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, useState, type ReactNode } from "react"
+import { createContext, useMemo, useState, type ReactNode } from "react"
+import { ThemeProvider } from "styled-components"
+
+import themes from "../../style/theme"
 
 interface PropsAppContext {
     children: ReactNode
@@ -15,11 +18,19 @@ export const Context = createContext<PropsContext> ({
 export default function AppContext({ children }: PropsAppContext) {
     const [theme, setTheme] = useState<'light' | 'dark'>('light')
 
+    type ThemeType = typeof themes.dark
+
+    const currentTheme = useMemo<ThemeType>(() => {
+        return themes[theme] || themes.light;
+    }, [themes])
+
     return (
         <Context value={{
             theme
         }}>
-            {children}
+            <ThemeProvider theme={currentTheme}>
+                {children}
+            </ThemeProvider>
         </Context>
     )
 }
