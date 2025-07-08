b0d94cd20b40dcfb5600b6d86c28cc4530aee8cf
Cauê Alves <cauealvesb4@gmail.com>
2025-07-07 21:00:37 -0300

    Terminando o Context do React

--- 2 - App de compras/app-compras/src/components/AppContext/AppContext.tsx
+++ 2 - App de compras/app-compras/src/components/AppContext/AppContext.tsx
@@ -1,16 +1,22 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import { ThemeProvider } from "styled-components"
 
 interface PropsContext {
     theme: 'light' | 'dark'
 }
 
-export const Context<>
+export const Context = createContext<PropsContext> ({
+    theme: 'light'
+})
 
 export default function AppContext() {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light')
+
     return (
-        <>
-        
-        </>
+        <Context value={{
+            theme
+        }}>
+
+        </Context>
     )
 }
