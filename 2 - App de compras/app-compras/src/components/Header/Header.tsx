13afb87b16e524a45dc17d17af4724bf358353d1
Cauê Alves <cauealvesb4@gmail.com>
2025-07-09 21:42:18 -0300

    Começando a estilização do Header

--- 2 - App de compras/app-compras/src/components/Header/Header.tsx
+++ 2 - App de compras/app-compras/src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import { List, Storefront, Scroll } from 'phosphor-react'
-import { ContainerHeader } from './style/Header'
+import { ContainerHeader, ContainerNameCompany } from './style/Header'
 
 export default function Header() {
     return (
         <ContainerHeader>
-            <div>
+            <ContainerNameCompany>
                 <List color='#fff' size={26} />
                 <h1>Empresa teste</h1>
-            </div> 
+            </ContainerNameCompany> 
             <div>
                 <Storefront color='#fff' size={26} />
                 <Scroll color='#fff' size={26} />
--- 2 - App de compras/app-compras/src/components/Header/style/Header.ts
+++ 2 - App de compras/app-compras/src/components/Header/style/Header.ts
@@ -3,3 +3,9 @@ import styled from "styled-components";
 export const ContainerHeader = styled.header`
     background-color: ${({ theme }) => theme.bgPrincipal};
 `;
+
+export const ContainerNameCompany = styled.div`
+    display: flex;
+    gap: 1em;
+    align-items: center;
+`;
