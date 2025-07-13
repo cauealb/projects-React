a6f7e3532617e11d5b2d2badc8696db82e7bd063
Cauê Alves <167707073+cauealb@users.noreply.github.com>
2025-07-13 20:57:35 -0300

    Update styled.d.ts

--- 2 - App de compras/app-compras/src/@types/styled.d.ts
+++ 2 - App de compras/app-compras/src/@types/styled.d.ts
@@ -3,10 +3,12 @@ import 'styled-components'
 declare module 'styled-components' {
     export interface DefaultTheme  {
         bgPrincipal: string
+        bgSecundary: string
         colorWhite: string
         colorBlack: string
         colorRed: string
         colorLines: string
         hoverBgPrincipal: string
+        hoverBgSecundary: string
     }
 }
