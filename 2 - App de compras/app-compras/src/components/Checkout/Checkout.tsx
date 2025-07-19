4b92d5755b8c8384906d3bb0a8d88f20a7f24c99
Cauê Alves <cauealvesb4@gmail.com>
2025-07-19 14:34:46 -0300

    Inciando estrutura semântica

--- 2 - App de compras/app-compras/src/components/Checkout/Checkout.tsx
+++ 2 - App de compras/app-compras/src/components/Checkout/Checkout.tsx
@@ -3,7 +3,12 @@
 export default function Checkout() {
     return(
         <>
-            
+            <div>
+                <header>
+                    <h2>Checkout</h2>
+                </header>
+                
+            </div>
         </>
     )
 }
